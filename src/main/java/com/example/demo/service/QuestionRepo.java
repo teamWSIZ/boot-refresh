05a7b413c091dc871e9dd47c80230399a8b684fe
P Marecki <pmarecki.pro@gmail.com>
2018-06-09 09:39:12 +0200

    sposoby migracji/tworzenia nowych systemów

--- src/main/java/com/example/demo/service/QuestionRepo.java
+++ src/main/java/com/example/demo/service/QuestionRepo.java
@@ -5,14 +5,22 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.repository.CrudRepository;
 
 import javax.transaction.Transactional;
+import java.util.Date;
 import java.util.List;
 
+//JPA
 public interface QuestionRepo extends CrudRepository<Question,Integer> {
+
     List<Question> findByActiveTrue();
+
     List<Question> findByTextStartingWith(String textPrefix);
+
     List<Question> findByTypeid(Integer typeid);
 
     List<Question> findByTypeidAndActiveTrue(Integer typeid);
+
+    List<Question> findByAnswer1EndingWith(String koniecA1);
+
 
     @Transactional
     @Modifying
