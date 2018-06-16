4070b8f3ce7e28616aaf991236f0470df3dd6729
P Marecki <pmarecki.pro@gmail.com>
2018-06-16 08:51:32 +0200

    remove fake data generator

--- src/main/java/com/example/demo/service/FakeDataGenerator.java
+++ src/main/java/com/example/demo/service/FakeDataGenerator.java
@@ -1,13 +1,12 @@
 package com.example.demo.service;
 
-import org.fluttercode.datafactory.impl.DataFactory;
 
 public class FakeDataGenerator {
     public static void main(String[] args) {
-        DataFactory df = new DataFactory();
-        for (int i = 0; i < 100; i++) {
-            String name = df.getFirstName() + " "+ df.getLastName() + " " + df.getCity();
-            System.out.println(name);
-        }
+//        DataFactory df = new DataFactory();
+//        for (int i = 0; i < 100; i++) {
+//            String name = df.getFirstName() + " "+ df.getLastName() + " " + df.getCity();
+//            System.out.println(name);
+//        }
     }
 }
